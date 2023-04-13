8ce7be2480b55d1f52d34e6d4507c40adaf6ee7e
Hanna E Song <hannaesong@localhost.localdomain>
2023-04-13 18:38:25 +0200

    Major update::Please revise logic

--- ckhutils/bool.py
+++ ckhutils/bool.py
@@ -5,7 +5,11 @@ def bool_output(seriesAff):
     '''
     Function to compare a healthy gait parameters with the gait parameters from subject
     '''
-    h_gait = pd.read_excel('healthy_gait.xlsx')
+    h_gait = pd.read_excel(
+        'healthy_gait.xlsx', engine="openpyxl",
+        usecols="A:B", nrows=12
+    )
+
     h_gait_series = h_gait.set_index('Gait Parameter')['Computed Mean']
     h_gait_series = h_gait_series.rename_axis(None)
     seriesAff.index = seriesAff.index.str.split('Aff').str[0]
